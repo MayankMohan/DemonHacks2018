b0961fec0e61b3f9eea1d90f59f9de848c117a0f
Chuck Wong <cwwong3@illinois.edu>
2018-10-21 00:25:09 -0500

    match files added, and make file for mac included

--- character.cpp
+++ character.cpp
@@ -10,19 +10,30 @@ bool Character::state(){
 	return stunned;
 }
 
-void move(sf::Event event){
-	if(event.type==sf::Event::KeyPressed){
+void move(int Keys){
+	/*if(event.type==sf::Event::KeyPressed){
+		switch (Keys){
+			case LEFT:
+				position.first -= Move;
+				break;
+			case :
+				position.first += Move;
+		}
 			if(event.key.code==sf::Keyboard::Left){
 				position.first-=5;
 			}
 			if(event.key.code==sf::Keyboard::Right){
 				position.first+=5;
 			}
-	}
+			Keys & LEFT
+	}*/
 	
 	
 }
-pair<double,double> Character::getPosition(){
+std::pair<double,double> Character::getPosition(){
 	return position;
 }
 
+sf::Rect<float> Character::getBound(){
+	return sf::Rect<float>(0,0,0,0);
+}
